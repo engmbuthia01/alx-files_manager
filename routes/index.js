7fee25183568e7bccc2b58a28b59f423aeeaafab
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-01-15 00:06:45 +0000

    user

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,6 @@ const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.post('/users', express.json(), UsersController.postNew);
+router.post('/users', UsersController.postNew);
 
 module.exports = router;
