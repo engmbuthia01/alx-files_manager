efea072d10aab92b4cba731db3cb0f00d68557ab
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-01-19 02:50:33 -0800

    new endpoints

--- utils/redis.js
+++ utils/redis.js
@@ -1,39 +1,23 @@
-#!/usr/bin/node
-
 const { createClient } = require('redis');
-const { promisify } = require('util');
 
 class RedisClient {
   constructor() {
     this.client = createClient();
-    this.client.on('error', (err) => console.log(err));
-    this.connected = false;
-    this.client.on('connect', () => {
-      this.connected = true;
-    });
+    this.client.on('error', (err) => console.error(`Redis Error: ${err}`));
+    this.client.connect();
   }
 
-  isAlive() {
-    return this.connected;
+  async set(key, value, duration) {
+    await this.client.set(key, value, { EX: duration });
   }
 
   async get(key) {
-    const getAsync = promisify(this.client.get).bind(this.client);
-    const val = await getAsync(key);
-    return val;
-  }
-
-  async set(key, val, dur) {
-    const setAsync = promisify(this.client.set).bind(this.client);
-    await setAsync(key, val, 'EX', dur);
+    return this.client.get(key);
   }
 
   async del(key) {
-    const delAsync = promisify(this.client.del).bind(this.client);
-    await delAsync(key);
+    await this.client.del(key);
   }
 }
 
-const redisClient = new RedisClient();
-
-module.exports = redisClient;
+module.exports = new RedisClient();
